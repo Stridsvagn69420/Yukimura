decae265435175f0d2beeb0eeee8d873b3a60b36
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-07-05 00:58:29 +0200

    Add LCM

--- src/base.rs
+++ src/base.rs
@@ -5,16 +5,37 @@
 /// Greatest Commom Divisor
 ///
 /// Gets the GCD of two numbers.
-/// This is used for Yukimura's Fraction datatype.
-pub fn greatest_common_divisor(a: i32, b: i32) -> i32 {
-    let mut a = a;
-    let mut b = b;
+/// 
+/// # Examples
+/// ```
+/// use yukimura::base;
+/// let result = base::gcd(72, 666);
+/// assert_eq!(result, 18);
+/// ```
+pub fn gcd(mut a: i32, mut b: i32) -> i32 {
     while a != 0 {
         let old_a = a;
         a = b % a;
         b = old_a;
     }
     b.abs()
+}
+
+
+/// Lowest Common Multiple
+/// 
+/// Gets the LCM of two numbers.
+/// It's literally just `a * b / gcd(a, b)`. That's the entire source code.
+/// Fucking amazing!
+/// 
+/// # Examples
+/// ```
+/// use yukimura::base;
+/// let result = base::lcm(20, 35);
+/// assert_eq!(result, 140);
+/// ```
+pub fn lcm(a: i32, b: i32) -> i32 {
+    a * b / gcd(a, b)
 }
 
 /// Add numbers in range
